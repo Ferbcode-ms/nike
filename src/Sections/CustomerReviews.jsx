b32cab7ae5e31a4a2bf775e7beb37a23dd279b3d
manpreet <manpreet26simmak@gmail.com>
2025-01-06 17:46:41 +0530

    first commit

--- src/Sections/CustomerReviews.jsx
+++ src/Sections/CustomerReviews.jsx
@@ -1,27 +1,30 @@
-import ReviewCard from "../Components/ReviewCard"
-import { reviews } from "../constants"
-
+import ReviewCard from "../Components/ReviewCard";
+import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
     <section className="max-container">
-      <h3 className="font-palanquin text-center text-[40px] sm:text-4xl font-bold">
+      <h3 className="text-center font-palanquin text-[40px] font-bold sm:text-4xl">
         What Our <span className="text-coral-red">Customer</span> Say ?
       </h3>
-      <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, illum voluptas. Ea?</p>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        illum v. E?
+      </p>
 
-      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+      <div className="mt-24 flex flex-1 items-center justify-evenly gap-14 max-lg:flex-col">
         {reviews.map((review) => (
           <ReviewCard
             key={review.customerName}
             imgURL={review.imgURL}
             customerName={review.customerName}
             rating={review.rating}
-            feedback={review.feedback} />
+            feedback={review.feedback}
+          />
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
